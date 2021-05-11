f635e18e2c8f6a5353b27846ec12657100c19bee
nepulepu <azrixanwar@gmail.com>
2021-05-11 22:16:41 +0800

    comit

--- LAB7/sequential_file.py
+++ LAB7/sequential_file.py
@@ -1,25 +1,39 @@
 def insert(file,files):
+    if (file[2]<0 or file[1]<0):
+        print()
+        print(file[0]+" size cannot be negative")
+        print()
+
     check=files[file[1]:file[1]+file[2]]
-    check2=[0 for i in range(len(check))]
+    check2=[None for i in range(len(check))]
     if file[1]>len(files) or (file[1]+file[2]>len(files)):
-        print("file will not be allocated")
+        print()
+        print("file \""+ file[0]+"\" will not be allocated")
+        print()
         return
     for i in range(file[1],file[1]+file[2]):
-        if check2 != check:
-            print("file is not allocated")
+        if check!=check2:
+            print()
+            print("file \""+ file[0]+"\" will not be allocated")
+            print()
             break
         else:
             files[i]=file[0]
 
-files=[0 for i in range(50)]
-print(files)
+files=[None for i in range(50)]
 
-file1 = ["cat",5,2]
-file2 = ["book",50,5]
-file3 = ["panzer iv H", 25,15]
+print()
+print("before:")
+print(files)
+print()
+file1 = ["cat",0,2]
+file2 = ["book",45,5]
+file3 = ["car", 5,15]
 
 insert(file1,files)
 insert(file2,files)
 insert(file3,files)
 
+print("after:")
 print(files)
+print()
